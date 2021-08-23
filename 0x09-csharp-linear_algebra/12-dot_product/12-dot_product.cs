d92756023e1df4a04d543f10d745f7cd1a83ac4b
Theemis <midinfotn401@gmail.com>
2021-08-23 10:16:52 +0100

    update

---
+++ 0x09-csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class VectorMath
+{
+    public static double DotProduct(double[] vector1, double[] vector2)
+    {
+        if (vector1.Length != vector2.Length ||
+         vector1.Length != 3 && vector1.Length != 2 ||
+         vector2.Length != 3 && vector2.Length != 2)
+        {
+            return -1;
+        }
+        double res =0 ;
+        for (int i = 0; i < vector1.Length; i++)
+        {
+            res += vector1[i] * vector2[i];
+        }
+        return res;
+    }
+}
+
