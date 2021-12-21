740bebc9cd17bababbbf9177cec43725c0f6fa4b
Theemis <midinfotn401@gmail.com>
2021-12-21 20:49:56 +0100

    update

--- 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -17,16 +17,14 @@ public Player(string name="Player", float maxHp=100f)
 
         this.name = name;
 
-    if(maxHp < 0)
+    if(maxHp <= 0)
     {
         Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
-        this.maxHp = 100f;
+        maxHp = 100f;
     }
-    else
-    {
         this.maxHp = maxHp;
-    }
-    this.hp = this.maxHp;
+
+        this.hp = maxHp;
 }
 /// <summary>
 /// display the hp of the playeer
