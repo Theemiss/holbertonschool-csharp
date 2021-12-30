24a4bfc99c25b3326dc47302db3f8f1cec128967
Theemiss <midinfotn401@gmail.com>
2021-12-30 13:40:40 +0100

    update

--- 0x0D-csharp-text_based_interface/InventoryLibrary/JSONStorage.cs
+++ 0x0D-csharp-text_based_interface/InventoryLibrary/JSONStorage.cs
@@ -5,7 +5,7 @@ using System.Text.Json;
 
 namespace JSONStorage
 {
-    class JSONStorage
+    public class JSONStorage
     {
         public Dictionary<string, dynamic> objects = new Dictionary<string, dynamic>();
 
@@ -28,13 +28,13 @@ namespace JSONStorage
         public void Save()
         {
             var options = new JsonSerializerOptions { WriteIndented = true };
-            string fileName = "../../../../storage/inventory_manager.json";
+            string fileName = "storage/inventory_manager.json";
             string jsonString = JsonSerializer.Serialize(objects, options);
             File.WriteAllText(fileName, jsonString);
         }
         public void Load()
         {
-            string fileName = "../../../../storage/inventory_manager.json";
+            string fileName = "storage/inventory_manager.json";
             string jsonString = File.ReadAllText(fileName);
             objects = JsonSerializer.Deserialize<Dictionary<string, dynamic>>(jsonString);
         }
